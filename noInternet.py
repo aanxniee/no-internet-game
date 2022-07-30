449fb8f746c6706ef76b00a84fe8a53ee841c373
Annie Cai <caishajia@gmail.com>
2022-07-30 18:15:02 -0400

    updated game loop/fps settings

--- noInternet.py
+++ noInternet.py
@@ -10,3 +10,17 @@ win = pygame.display.set_mode((screenLength, screenHeight))
 
 clock = pygame.time.Clock()
 
+run = True
+while run:
+    for event in pygame.event.get():
+        # allows user to close window
+        if event.type == pygame.QUIT:
+            run = False
+
+    clock.tick(30) # 30 fps
+    pygame.display.update()
+
+
+pygame.quit()
+
+
