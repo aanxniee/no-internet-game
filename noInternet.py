1d5f49c3bbcd2f17d608d66396d658a3e88cd949
Annie Cai <caishajia@gmail.com>
2022-07-30 18:05:22 -0400

    initial setup of game file

---
+++ noInternet.py
@@ -0,0 +1,12 @@
+import pygame, random
+
+pygame.init()
+
+screenLength = 600;
+screenHeight = 300;
+
+# double parenthesis for coordinates
+win = pygame.display.set_mode((screenLength, screenHeight))
+
+clock = pygame.time.Clock()
+
