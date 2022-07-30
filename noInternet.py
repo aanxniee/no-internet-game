2414906ca93c6f3efb75f682621a14cc60516c83
Annie Cai <caishajia@gmail.com>
2022-07-30 18:31:46 -0400

    added jump function to dinosaur

--- noInternet.py
+++ noInternet.py
@@ -17,21 +17,39 @@ class Dinosaur(object):
         self.h = 50
         self.x = 100
         self.y = screenHeight - 150
+        self.jumping = False
+        self.jump_increment = 0
 
     def draw(self):
         pygame.draw.rect(win, (125,125,125), (self.x, self.y, self.w, self.h))
 
-player = Dinosaur()
+    def move(self):
+        keys = pygame.key.get_pressed()
+
+        if (keys[pygame.K_UP] or keys[pygame.K_SPACE]) and not self.jumping: # detects jumping
+            self.jumping = True
+
+        if self.jumping: # parabolic jump
+            if self.jump_increment <= 20:
+                self.y -= (10 - self.jump_increment) * 2
+                self.jump_increment += 1
+            else:
+                self.jump_increment = 0
+                self.jumping = False
+
+player = Dinosaur() # create dinosaur object
 
 run = True
 while run:
     win.fill((255,255,255)) # fill window to white
+    pygame.draw.rect(win, (0,0,0), (0, screenHeight-100, screenLength, 100))
     for event in pygame.event.get():
         # allows user to close window
         if event.type == pygame.QUIT:
             run = False
 
     player.draw()
+    player.move()
 
     clock.tick(30) # 30 fps
     pygame.display.update()
